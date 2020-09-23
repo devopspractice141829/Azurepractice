f6ff614137330e1d2b5109e79600923bc5b8fc73
raju <devopspractice141829@gmail.com>
2020-09-23 10:21:11 +0530

    commiting the code

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "../index.css";
+
+const footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <span> by </span>
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://github.com/afozbek"
+      >
+        Sandeep Gaonkar
+      </a>
+    </div>
+  );
+};
+
+export default footer;
